c4098e6690ed425eb143b53f6f95f1d7d67664fb
intsamoilov <int.samoilov@gmail.com>
2018-11-26 16:16:09 +0300

    Исправил замечания

--- js/map.js
+++ js/map.js
@@ -1,10 +1,11 @@
 'use strict';
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
+var AD_COUNT = 8;
 var PIN_WIDTH = 50;
 var PIN_HEIGHT = 70;
 var PIN_IMG_WIDTH = 40;
 var PIN_IMG_HEIGHT = 40;
-var PIN_MIN_Y = 200;
+var PIN_MIN_Y = 130;
 var PIN_MAX_Y = 630;
 var PIN_MIN_X = 300;
 var PIN_MAX_X = 900;
@@ -12,6 +13,7 @@ var PIN_ALT = 'Метка объявления';
 var PRICE_MIN = 1000;
 var PRICE_MAX = 1000000;
 var MAX_ROOMS = 5;
+var MAX_GUESTS = 15;
 var OFFER_IMAGE_WIDTH = 45;
 var OFFER_IMAGE_HEIGHT = 40;
 var OFFER_IMAGE_ALT = 'Фотография жилья';
@@ -24,6 +26,11 @@ var OFFER_TITLES = [
   'Некрасивый негостеприимный домик',
   'Уютное бунгало далеко от моря',
   'Неуютное бунгало по колено в воде'];
+var TYPES = {
+  'palace': 'Дворец',
+  'flat': 'Квартира',
+  'house': 'Дом',
+  'bungalo': 'Бунгало'};
 var OFFER_TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var OFFER_TIMES = ['12:00', '13:00', '14:00'];
 var OFFER_FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
@@ -31,26 +38,26 @@ var OFFER_PHOTOS = [
   'http://o0.github.io/assets/images/tokyo/hotel1.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel2.jpg',
   'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var getRandomNum = function (min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var getFeatures = function (array) {
   var cutArray = [];
-  var sortArrey = getRandomArray(array);
-  var index = getRandomNum(0, array.length-1);
+  var sortArrey = getShuffleArray(array);
+  var index = getRandomNum(0, array.length - 1);
   for (var i = 0; i <= index; i++) {
     cutArray[i] = sortArrey[i];
   }
   return cutArray;
-}
-//----------------------------------------------------------------------------
-var getRandomArray = function (array) {
+};
+// ----------------------------------------------------------------------------
+var getShuffleArray = function (array) {
   var randomArray = [];
   for (var i = 0; i < array.length; i++) {
-  var index = getRandomNum(1, array.length);
-  randomArray[i] = array[index-1];
+    var index = getRandomNum(1, array.length);
+    randomArray[i] = array[index - 1];
     for (var j = 0; j < i; j++) {
       if (randomArray[j] === randomArray[i]) {
         delete randomArray[i];
@@ -60,11 +67,9 @@ var getRandomArray = function (array) {
   }
   return randomArray;
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var renderFeatures = function (card, element) {
-  while(element.querySelector('.popup__feature')) {
-    element.querySelector('.popup__feature').remove();
-  }
+  element.querySelector('.popup__features').innerHTML = '';
   for (var i = 0; i < card.offer.features.length; i++) {
     var li = document.createElement('li');
     li.classList.add('popup__feature');
@@ -72,11 +77,9 @@ var renderFeatures = function (card, element) {
     element.querySelector('.popup__features').appendChild(li);
   }
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var renderPhotos = function (card, element) {
-  while(element.querySelector('.popup__photo')) {
-    element.querySelector('.popup__photo').remove();
-  }
+  element.querySelector('.popup__photos').innerHTML = '';
   for (var i = 0; i < card.offer.photos.length; i++) {
     var img = document.createElement('img');
     img.classList.add('.popup__photo');
@@ -87,13 +90,13 @@ var renderPhotos = function (card, element) {
     element.querySelector('.popup__photos').appendChild(img);
   }
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var renderCard = function (card) {
   var cardTemplate = document.querySelector('#card').content.cloneNode(true);
   cardTemplate.querySelector('.popup__title').textContent = card.offer.title;
   cardTemplate.querySelector('.popup__text--address').textContent = card.offer.address;
   cardTemplate.querySelector('.popup__text--price').textContent = card.offer.price;
-  cardTemplate.querySelector('.popup__type').textContent = card.offer.type;
+  cardTemplate.querySelector('.popup__type').textContent = TYPES[card.offer.type];
   cardTemplate.querySelector('.popup__text--capacity').textContent =
     card.offer.rooms + ' комнаты для ' + card.offer.guests + ' гостей';
   cardTemplate.querySelector('.popup__text--time').textContent =
@@ -103,14 +106,14 @@ var renderCard = function (card) {
   renderPhotos(card, cardTemplate);
   cardTemplate.querySelector('.popup__avatar').src = card.author.image;
   document.querySelector('.map').insertBefore(cardTemplate,
-    document.querySelector('.map__filters-container'));
+      document.querySelector('.map__filters-container'));
 
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var renderPins = function (cards) {
   var fragment = document.createDocumentFragment();
   var pin = document.querySelector('.map__pins');
-  for (var i = 0; i < cards.length; i ++) {
+  for (var i = 0; i < cards.length; i++) {
     var div = document.createElement('div');
     var img = document.createElement('img');
     div.classList.add('map__pin');
@@ -126,31 +129,34 @@ var renderPins = function (cards) {
   }
   pin.appendChild(fragment);
 };
-//----------------------------------------------------------------------------
-  var getDataArray = function (count) {
+// ----------------------------------------------------------------------------
+var getDataArray = function () {
   var result = [];
-  var randomTitles = [];
-
-  randomTitles = getRandomArray(OFFER_TITLES);
-  for (var i = 1; i <= count; i++) {
+  var authors = [];
+  for (var j = 0; j < AD_COUNT; j++) {
+    authors[j] = 'img/avatars/user0' + (j + 1) + '.png';
+  }
+  authors = getShuffleArray(authors);
+  var randomTitles = getShuffleArray(OFFER_TITLES);
+  for (var i = 0; i < AD_COUNT; i++) {
     var posX = getRandomNum(PIN_MIN_X, PIN_MAX_X) - Math.round(PIN_WIDTH / 2);
     var posY = getRandomNum(PIN_MIN_Y, PIN_MAX_Y) - PIN_HEIGHT;
     result.push({
       author: {
-        image: 'img/avatars/user0' + i + '.png'
+        image: authors[i]
       },
       offer: {
-        title: randomTitles[i-1],
+        title: randomTitles[i],
         address: posX + ', ' + posY,
         price: getRandomNum(PRICE_MIN, PRICE_MAX) + ' ₽/ночь',
         type: OFFER_TYPES[getRandomNum(1, OFFER_TYPES.length) - 1],
         rooms: getRandomNum(1, MAX_ROOMS),
-        guests: getRandomNum(1, 15),
+        guests: getRandomNum(1, MAX_GUESTS),
         checkin: OFFER_TIMES[getRandomNum(1, OFFER_TIMES.length) - 1],
         checkout: OFFER_TIMES[getRandomNum(1, OFFER_TIMES.length) - 1],
         features: getFeatures(OFFER_FEATURES),
         description: '',
-        photos: getRandomArray(OFFER_PHOTOS)
+        photos: getShuffleArray(OFFER_PHOTOS)
       },
       location: {
         x: posX,
@@ -160,16 +166,14 @@ var renderPins = function (cards) {
   }
   return result;
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 var initiate = function () {
-  var adCount = 8;
-  var adCards = [];
+  var adCards = getDataArray();
   var map = document.querySelector('.map');
   map.classList.remove('map--faded');
-  adCards = getDataArray(adCount);
   renderPins(adCards);
   renderCard(adCards[0]);
 };
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
 initiate();
-//----------------------------------------------------------------------------
+// ----------------------------------------------------------------------------
